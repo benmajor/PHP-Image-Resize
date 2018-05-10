3679ad6755be7a9e56097a2e8c5cec25e99d7dd7
Ben Major <ben.major88@gmail.com>
2018-05-10 09:38:31 +0100

    Create Sepia.php

---
+++ examples/Sepia.php
@@ -0,0 +1,19 @@
+<?php
+
+# Don't resize, but create sepia effect:
+
+use \BenMajor\ImageResize;
+
+require 'vendor/autoload.php';
+
+# Generate a new image resize object using a remote image:
+$image = new ImageResize('http://4.bp.blogspot.com/-wykO_6QC7Sk/Uemq1_yXg8I/AAAAAAAAAHY/dtBq0IneP08/s1600/maka.jpg');
+
+# Make it greyscale:
+$image->greyscale();
+
+# Colorize to simulate sepia:
+$image->colorize('#332600');
+
+# Output it to the browser:
+$image->output();
