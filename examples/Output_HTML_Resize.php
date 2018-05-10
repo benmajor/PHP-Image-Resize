ce7f151e65c128803a2f8dd3183f8e71c5521c9b
Ben Major <ben.major88@gmail.com>
2018-05-10 09:50:35 +0100

    Create Output_HTML_Resize.php

---
+++ examples/Output_HTML_Resize.php
@@ -0,0 +1,16 @@
+<?php
+
+# Maintain transparent background:
+
+use \BenMajor\ImageResize;
+
+require 'vendor/autoload.php';
+
+# Generate a new image resize object using a remote image:
+$image = new ImageResize('http://4.bp.blogspot.com/-wykO_6QC7Sk/Uemq1_yXg8I/AAAAAAAAAHY/dtBq0IneP08/s1600/maka.jpg');
+
+# Resize crop:
+$image->resizeCrop(200,200);
+
+# Generate a HTML tag and echo it - alt: My Alt, title: My Title
+$image->outputHTML( true, 'My Alt', 'My Title' );
