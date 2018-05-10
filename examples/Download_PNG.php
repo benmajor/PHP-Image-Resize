ade6f43521a1fa145aac3006fa9cd8234ec7097f
Ben Major <ben.major88@gmail.com>
2018-05-10 09:48:21 +0100

    Create Download_PNG.php

---
+++ examples/Download_PNG.php
@@ -0,0 +1,13 @@
+<?php
+
+# Maintain transparent background:
+
+use \BenMajor\ImageResize;
+
+require 'vendor/autoload.php';
+
+# Generate a new image resize object using a remote image:
+$image = new ImageResize('http://4.bp.blogspot.com/-wykO_6QC7Sk/Uemq1_yXg8I/AAAAAAAAAHY/dtBq0IneP08/s1600/maka.jpg');
+
+# Force download as a PNG (with test.png as the filename):
+$image->downloadPNG('test.png');
