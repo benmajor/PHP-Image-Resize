e441871f6ee0d133cd65590f3ef09cd973d82d24
Ben Major <ben.major88@gmail.com>
2018-05-10 09:45:40 +0100

    Create Transparent_Replace.php

---
+++ examples/Transparent_Replace.php
@@ -0,0 +1,22 @@
+<?php
+
+# Replace transparent background with a colour:
+
+use \BenMajor\ImageResize;
+
+require 'vendor/autoload.php';
+
+# Generate a new image resize object using a remote image:
+$image = new ImageResize('http://vignette.wikia.nocookie.net/rickandmorty/images/1/19/Pickle_rick_transparent.png/revision/latest/scale-to-width-down/363?cb=20171025014216');
+
+# Turn off transparency:
+$image->setTransparency( false );
+
+# Set the background:
+$image->setBackgroundColor('#ff00ff');
+
+# Resize it to 300px wide:
+$image->resizeWidth(300);
+
+# Output it to the browser:
+$image->output();
