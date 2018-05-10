da123a294872c7303fe13c28bf528db424b9554c
Ben Major <ben.major88@gmail.com>
2018-05-10 09:36:48 +0100

    Create Contain_Border.php

---
+++ examples/Contain_Border.php
@@ -0,0 +1,26 @@
+<?php
+
+# Contain the image on a 500x500 pixel canvas:
+
+use \BenMajor\ImageResize;
+
+require 'vendor/autoload.php';
+
+# Generate a new image resize object using a remote image:
+$image = new ImageResize('http://4.bp.blogspot.com/-wykO_6QC7Sk/Uemq1_yXg8I/AAAAAAAAAHY/dtBq0IneP08/s1600/maka.jpg');
+
+# Set transparency to false (because we're using JPEG),
+# and setting to tru would force a black background:
+$image->setTransparency( false );
+
+# Set the background to white:
+$image->setBackgroundColor('#333');
+
+# Add a 5px white border:
+$image->setBorder( 5, '#fff' );
+
+# Contain the image:
+$image->contain( 500, 500 );
+
+# Output it to the browser:
+$image->output();
