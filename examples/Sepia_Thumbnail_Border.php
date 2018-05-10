c22e6f6710038df72c5d89e5636488911c202b34
Ben Major <ben.major88@gmail.com>
2018-05-10 09:39:46 +0100

    Create Sepia_Thumbnail_Border.php

---
+++ examples/Sepia_Thumbnail_Border.php
@@ -0,0 +1,25 @@
+<?php
+
+# Create a 300x200 sepia thumbnail with a border.
+
+use \BenMajor\ImageResize;
+
+require 'vendor/autoload.php';
+
+# Generate a new image resize object using a remote image:
+$image = new ImageResize('http://4.bp.blogspot.com/-wykO_6QC7Sk/Uemq1_yXg8I/AAAAAAAAAHY/dtBq0IneP08/s1600/maka.jpg');
+
+# Make it greyscale:
+$image->greyscale();
+
+# Colorize to simulate sepia:
+$image->colorize('#332600');
+
+# Add the border:
+$image->setBorder( 5, '#333' );
+
+# Resize and crop:
+$image->resizeCrop( 300, 200 );
+
+# Output it to the browser:
+$image->output();
