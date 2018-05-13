1d0f685dd9182cc21c8f7c92c259f4c7479a1683
Ben Major <ben.major88@gmail.com>
2018-05-13 13:43:05 +0100

    Update Thumbnail_Square.php

--- examples/Thumbnail_Square.php
+++ examples/Thumbnail_Square.php
@@ -7,7 +7,7 @@ use \BenMajor\ImageResize\Image;
 require 'vendor/autoload.php';
 
 # Generate a new image resize object using a remote image:
-$image = new ImageResize('http://4.bp.blogspot.com/-wykO_6QC7Sk/Uemq1_yXg8I/AAAAAAAAAHY/dtBq0IneP08/s1600/maka.jpg');
+$image = new Image('http://4.bp.blogspot.com/-wykO_6QC7Sk/Uemq1_yXg8I/AAAAAAAAAHY/dtBq0IneP08/s1600/maka.jpg');
 
 # Resize it (will produce a 200x200 thumb):
 $image->resizeCrop( 200 );
