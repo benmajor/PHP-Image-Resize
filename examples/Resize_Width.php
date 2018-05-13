4333449816406692911d543a03bdb83df808fab8
Ben Major <ben.major88@gmail.com>
2018-05-13 13:42:35 +0100

    Update Resize_Width.php

--- examples/Resize_Width.php
+++ examples/Resize_Width.php
@@ -7,7 +7,7 @@ use \BenMajor\ImageResize\Image;
 require 'vendor/autoload.php';
 
 # Generate a new image resize object using a remote image:
-$image = new ImageResize('http://4.bp.blogspot.com/-wykO_6QC7Sk/Uemq1_yXg8I/AAAAAAAAAHY/dtBq0IneP08/s1600/maka.jpg');
+$image = new Image('http://4.bp.blogspot.com/-wykO_6QC7Sk/Uemq1_yXg8I/AAAAAAAAAHY/dtBq0IneP08/s1600/maka.jpg');
 
 # Resize and constrain:
 $image->resizeWidth( 300 );
