1c051dfac2c0935fdc9d88975548727c18cae3e1
Ben Major <ben.major88@gmail.com>
2018-05-13 13:42:02 +0100

    Update Output_HTML.php

--- examples/Output_HTML.php
+++ examples/Output_HTML.php
@@ -7,7 +7,7 @@ use \BenMajor\ImageResize\Image;
 require 'vendor/autoload.php';
 
 # Generate a new image resize object using a remote image:
-$image = new ImageResize('http://4.bp.blogspot.com/-wykO_6QC7Sk/Uemq1_yXg8I/AAAAAAAAAHY/dtBq0IneP08/s1600/maka.jpg');
+$image = new Image('http://4.bp.blogspot.com/-wykO_6QC7Sk/Uemq1_yXg8I/AAAAAAAAAHY/dtBq0IneP08/s1600/maka.jpg');
 
 # Generate a HTML tag and echo it - alt: My Alt, title: My Title
 $image->outputHTML( true, 'My Alt', 'My Title' );
