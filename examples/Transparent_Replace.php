c150b614884318e5474136cfea3fe7dcb2e6522f
Ben Major <ben.major88@gmail.com>
2018-05-13 13:43:51 +0100

    Update Transparent_Replace.php

--- examples/Transparent_Replace.php
+++ examples/Transparent_Replace.php
@@ -7,7 +7,7 @@ use \BenMajor\ImageResize\Image;
 require 'vendor/autoload.php';
 
 # Generate a new image resize object using a remote image:
-$image = new ImageResize('http://vignette.wikia.nocookie.net/rickandmorty/images/1/19/Pickle_rick_transparent.png/revision/latest/scale-to-width-down/363?cb=20171025014216');
+$image = new Image('http://vignette.wikia.nocookie.net/rickandmorty/images/1/19/Pickle_rick_transparent.png/revision/latest/scale-to-width-down/363?cb=20171025014216');
 
 # Turn off transparency:
 $image->setTransparency( false );
